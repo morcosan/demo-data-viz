f3f0e974dacf7decc359c3ac9b3b9e0a6406bc98
Morco <hello.morco@gmail.com>
2025-12-19 21:26:32 +0200

    [#1] Create basic app setup with next.js

---
+++ app/prettier.config.js
@@ -0,0 +1,34 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+	arrowParens: 'always',
+	bracketSameLine: false,
+	bracketSpacing: true,
+	embeddedLanguageFormatting: 'auto',
+	endOfLine: 'lf',
+	htmlWhitespaceSensitivity: 'css',
+	printWidth: 115, // Approx 120 (tabs are counted as 1, but rendered as 2/3/4)
+	semi: false,
+	singleQuote: true,
+	trailingComma: 'es5',
+	useTabs: true,
+
+	// Format SVG
+	overrides: [{ files: '*.svg', options: { parser: 'html' } }],
+
+	// Plugins
+	plugins: ['prettier-plugin-css-order', 'prettier-plugin-organize-imports', 'prettier-plugin-tailwindcss'],
+
+	// Plugin: css-order
+	cssDeclarationSorterOrder: 'concentric-css',
+	cssDeclarationSorterCustomOrder: [],
+	cssDeclarationSorterKeepOverrides: false,
+
+	// Plugin: tailwindcss
+	// tailwindConfig: './tailwind.config.ts',
+	tailwindFunctions: ['clsx', 'cx'],
+}
+
+export default config
