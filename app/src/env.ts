143f15ac8ad3715bead882a6c728af942c601369
Morco <hello.morco@gmail.com>
2026-01-27 14:04:52 +0200

    Comment build number

--- app/src/env.ts
+++ app/src/env.ts
@@ -2,4 +2,4 @@ globalThis.ENV__BASE_PATH = process.env.ENV__BASE_PATH || ''
 globalThis.ENV__BUILD_NUMBER = process.env.ENV__BUILD_NUMBER || '0'
 
 export const BASE_PATH = ENV__BASE_PATH
-export const BUILD_NUMBER = ENV__BUILD_NUMBER
+// export const BUILD_NUMBER = ENV__BUILD_NUMBER
